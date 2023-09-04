8445cc5d5168330c2b05c54868d2a8cd8ab58083
abiyasa05 <abiyasap24@gmail.com>
2023-09-04 15:24:35 +0700

    add: bakground color white in tab bar

--- basic_ui/lib/pages/home_page.dart
+++ basic_ui/lib/pages/home_page.dart
@@ -3,31 +3,42 @@ import 'package:basic_ui/layouts/home/home_main_layout.dart';
 import 'package:basic_ui/layouts/home/home_today_layout.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: true,
-          title: const Text("My App"),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                child: Text("Berita Tebaru"),
-              ),
-              Tab(
-                child: Text("Pertandingan Hari ini"),
-              ),
-            ],
-          ),
-        ),
-        body: const TabBarView(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: const Text("My App"),
+        backgroundColor: Colors.red,
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: Column(
           children: [
-            HomeMainLayout(),
-            HomeTodayLayout(),
+            Container(
+              constraints: BoxConstraints.expand(
+                  height: 50), // Adjust the height as needed
+              child: TabBar(
+                tabs: [
+                  Tab(
+                    child: Text("Berita Tebaru"),
+                  ),
+                  Tab(
+                    child: Text("Pertandingan Hari ini"),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  HomeMainLayout(),
+                  HomeTodayLayout(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
